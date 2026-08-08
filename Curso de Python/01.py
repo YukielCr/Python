f4ca1d054bd9ee131d8a3a3a22b8e2a7303643ee
Yukiel <140352744+YukielCr@users.noreply.github.com>
2026-08-08 13:10:25 -0600

    Retos de Progracion de Pithon

---
+++ Curso de Python/01.py
@@ -0,0 +1,11 @@
+#Programa: El area del trapecio con lectura aleatoria
+#Autor: Fernando Uriel Corona Ramos
+#Carrera: Ingenieria Quimica
+#Fecha: 20 de febrero del 2026
+#Version: 3.14.3
+
+import random
+
+#randint
+num=random.randint(1, 100)
+print("num= random.randint(1, 100) =", num)
